645442c27d547bd5f20d2ff68c2608903f5c270f
emili <emilie0bourret@gmail.com>
2020-10-29 17:14:29 +0100

    user commit

--- app/src/main/java/com/example/appden3/NewUserActivity.java
+++ app/src/main/java/com/example/appden3/NewUserActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class NewUserActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
