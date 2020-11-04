7c3cfb5520de1570e5d1f66936365f08cdccee47
Lucie-Masson <masson-lucie@laposte.net>
2020-11-04 17:45:41 +0100

    Page des résultats

--- app/src/main/java/com/example/appden3/ReponsesActivity.java
+++ app/src/main/java/com/example/appden3/ReponsesActivity.java
@@ -2,15 +2,20 @@ package com.example.appden3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ReponsesActivity extends AppCompatActivity {
 
-    private float tauxAlcool;
+    private Double tauxAlcool;
     private boolean recidive;
-    private String user;
+    private boolean probatoire;
+    private int points;
+    private TextView reponse;
+    private Button recommencer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,9 +23,20 @@ public class ReponsesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reponses);
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
-        if(extras != null) tauxAlcool = extras.getFloat("tauxAlcool");
+        if(extras != null) tauxAlcool = extras.getDouble("tauxAlcool");
         if(extras != null) recidive = extras.getBoolean("recidive");
-        if(extras != null) user = extras.getString("user");
+        if(extras != null) probatoire = extras.getBoolean("userProbatoire");
+        if(extras != null) points = extras.getInt("userPoints");
+        reponse = findViewById(R.id.text_resultat);
+        recommencer = findViewById(R.id.button_recommencer);
+
+        sanction(tauxAlcool,recidive,probatoire, points);
+    }
+
+    private void sanction(Double tauxAlcool, boolean recidive, boolean probatoire, int points) {
+        if (points <= 6){
+
+        }
     }
 
 
