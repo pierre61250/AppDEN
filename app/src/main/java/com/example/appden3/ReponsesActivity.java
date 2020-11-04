1cd1e3cce721a6ecfbf395dbd81dc0b094c2868e
Lucie-Masson <masson-lucie@laposte.net>
2020-11-04 17:10:08 +0100

    Page des résultats

--- app/src/main/java/com/example/appden3/ReponsesActivity.java
+++ app/src/main/java/com/example/appden3/ReponsesActivity.java
@@ -1,5 +1,6 @@
 package com.example.appden3;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -7,9 +8,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ReponsesActivity extends AppCompatActivity {
 
+    private float tauxAlcool;
+    private boolean recidive;
+    private String user;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reponses);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if(extras != null) tauxAlcool = extras.getFloat("tauxAlcool", 0);
+        if(extras != null) recidive = extras.getBoolean("recidive", false);
     }
+
+
 }
